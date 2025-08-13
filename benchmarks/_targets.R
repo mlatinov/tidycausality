590d95017af8b03ffc349b8f0c93153da11464bd
mlatinov <latinovmetodi@gmail.com>
2025-08-13 23:05:29 +0300

    start benchmarking with targets

---
+++ benchmarks/_targets.R
@@ -0,0 +1,27 @@
+
+#### Targets Master Script ####
+
+### Libraries ###
+
+# Organization
+library(targets)
+library(tarchetypes)
+
+# Data Wrangling
+library(tidyverse)
+
+# Modeling
+library(tidymodels)
+library(tidycausality)
+
+
+
+## Pipeline ##
+list(
+
+  # Get dataset for Benchmarking
+  tar_target(
+    name = ,
+    command =
+  )
+)
