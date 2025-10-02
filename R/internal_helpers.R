8a1d0e80b5f4ed42221114185cc2b740c772d30d
Metodi Latinov <metodilation@abv.bg>
2025-10-02 18:20:03 +0300

    Add functions to support U and RX

--- R/internal_helpers.R
+++ R/internal_helpers.R
@@ -53,10 +53,46 @@
   return(data_resid)
 }
 
+#' Function to apply Residualization for TSRI leaner
+#' @keywords internal
+.tsri_residualization <- function(data, m_hat, e_hat, outcome_name, treatment){
+  data_resid <- data %>%
+    mutate(
+      R_A = .data[[treatment]] - m_hat
+    )
+  # Return data_resid
+  return(data_resid)
+}
+
+#' Function to apply Residualization for U leaner
+#' @keywords internal
+.u_residualization <- function(data, m_hat, e_hat, outcome_name, treatment){
+  # Residualization for the U learner Compute U Score
+  data_resid <- data %>%
+    mutate(
+      U = (.data[[outcome_name]] - m_hat) / (.data[[treatment]] - e_hat)
+    )
+}
+
+#' Function to apply Residualization for RX  leaner
+#' @keywords internal
+.rx_residualization <- function(data, m_hat, e_hat, outcome_name, treatment){
+  data_resid <- data %>%
+    mutate(
+      Y_tilda = .data[[outcome_name]] - m_hat, # residualized outcome
+      A_tilda = .data[[treatment]] - e_hat, # residualized treatment
+      w = sqrt(abs(A_tilda))               # compute the weights
+    )
+  # Return data_resid
+  return(data_resid)
+}
 # Dispatch Table for Residualization
 .dispatch_table_residualization <- list(
   "dr_learner" = .dr_residualization,
-  "r_learner" = .r_residualization
+  "r_learner" = .r_residualization,
+  "tsri_learner" = .tsri_residualization,
+  "u_learner" = .u_residualization,
+  "rx_learner" = .rx_residualization
 )
 
 #' Function to apply Residualization for every model that needed it
@@ -76,7 +112,7 @@
   # Recipe
   recipe <- recipe(Y_tilde ~ A_tilde + ., data = data) %>%
     # Remove the original treatment and outcome columns
-    step_rm(treatment,outcome)
+    step_rm(treatment,outcome) %>%
   # Add interaction terms
   step_interact(terms = ~ A_tilde:all_predictors())
 
@@ -123,10 +159,66 @@
   ))
 }
 
+#' Function for second stage predictions with U learner
+#' @keywords internal
+.u_second_stage <- function(rf_spec,data, m_hat,outcome,treatment){
+  # Recipe
+  recipe <- recipe(U ~ ., data = data) %>%
+    # Remove the original treatment and outcome columns
+    step_rm(treatment,outcome)
+  # RF Workflow
+  rf_wf <- workflow() %>%
+    add_model(rf_spec) %>%
+    add_recipe(recipe)
+
+  # Predict residual outcome
+  tau <- predict(rf_wf, new_data = data) %>% pull(.pred)
+  # Reconstruct counterfactual outcomes
+  y1 <- m_hat$m_hat + tau
+  y0 <- m_hat$m_hat
+
+  # Return list with Y1 and Y0
+  return(list(
+    y1 = y1,
+    y0 = y0
+  ))
+}
+#' Function for second stage predictions with RX learner
+#' @keywords internal
+.rx_second_stage <- function(rf_spec,data, m_hat,outcome,treatment){
+  # Recipe
+  recipe <- recipe(Y_tilde ~ A_tilde + ., data = data) %>%
+    # Remove the original treatment and outcome columns
+    step_rm(treatment,outcome,w) %>%
+  # Add interaction terms
+  step_interact(terms = ~ A_tilde:all_predictors())
+
+  # RF Workflow
+  rf_wf <- workflow() %>%
+    add_model(rf_spec) %>%
+    add_recipe(recipe)
+
+  # RF fit model with weights
+  rf_fit <- rf_wf %>% fit(data = data, case_weights = data$w)
+
+  # Predict residual outcome
+  tau <- predict(rf_fit, new_data = data) %>% pull(.pred)
+  # Reconstruct counterfactual outcomes
+  y1 <- m_hat + tau
+  y0 <- m_hat
+
+  # Return list with Y1 and Y0
+  return(list(
+    y1 = y1,
+    y0 = y0
+  ))
+}
 # Dispatch Table for Second stage predictions models
 .dispatch_table_second_stage <- list(
   "dr_learner" = .dr_second_stage,
-  "r_learner" = .r_second_stage
+  "r_learner" = .r_second_stage,
+  "u_learner" = .u_second_stage,
+  "rx_learner" = .rx_second_stage
 )
 
 #'  Function to make a second stage regression models for predictions
