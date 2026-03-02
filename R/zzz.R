48bae0fd9c990d4289ddf6080c740d93412e9ddb
Metodi Latinov <latinovmetodi@gmail.com>
2026-03-02 19:21:49 +0200

    Create zzz.R with package documentation

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+#' tidycausality: Causal Inference Modeling with Parsnip
+#'
+#' A wrapper for causal machine learning models integrated into the 'parsnip' modeling framework.
+#'
+#' @docType package
+#' @name tidycausality
+#' @import tidymodels
+#' @import tidyverse
+NULL
